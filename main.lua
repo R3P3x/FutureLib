faeabc1a691e555bab731f657267992e419197f2
Future_Internals <ccbig2018@gmail.com>
2024-12-08 19:17:21 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+local function tobecreated()
+  warn("libraries set to tobecreated are libraries that are planned/confirmed to be added in the future but i dont have the time to do it yet")
+end
+
+local lib = {}
+
+lib.ESP = tobecreated()
+
+return lib
