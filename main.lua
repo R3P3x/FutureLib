46b8d53d642b98136142f07f1a11609ff084f860
Future_Internals <ccbig2018@gmail.com>
2024-12-08 20:13:20 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,21 @@
 local function tobecreated()
-  warn("libraries set to tobecreated are libraries that are planned/confirmed to be added in the future but i dont have the time to do it yet")
+  warn("libraries set to tobecreated are libraries that are planned/confirmed to be added in the future but i dont have the time to do it yet, if you see this warn in console it means you called a lib that hasnt been added yet.")
 end
 
 local lib = {}
 
-lib.ESP = tobecreated()
+lib.ESP = {}
+
+lib.ESP.UsernameESP = require(game:HttpGet(https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/UsernameESP.lua))
+
+lib.Notification = {}
+
+lib.Notification.AbyssNotifications = require(game:HttpGet(https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/abyssNotifications.lua))
+
+lib.UI = {}
+
+lib.UI.Fluent = require(game:HttpGet(https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/Fluent.lua))
+
+lib.UI.0x37 = require(game:HttpGet(https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/0x37.lua))
 
 return lib
