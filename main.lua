1ef81dd15e14647b728db3bbcf0481955a51c38e
SB_Scripts <st3v3bl0ks@gmail.com>
2025-02-18 18:46:51 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,29 +1,29 @@
 --[[
-  Library loadstring: loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/main.lua"))()
+  Library loadstring: loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/main.lua"))()
 ]]
 
 local lib = {}
 
 lib.ESP = {}
 
-lib.ESP.NameESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/NameESP.lua"))()
+lib.ESP.NameESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/NameESP.lua"))()
 
-lib.ESP.Chams = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/chams.lua"))()
+lib.ESP.Chams = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/chams.lua"))()
 
-lib.ESP.HighlightService = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/highlightService.lua"))()
+lib.ESP.HighlightService = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/highlightService.lua"))()
 
 lib.Notification = {}
 
-lib.Notification.AbyssNotifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/abyssNotifications.lua"))()
+lib.Notification.AbyssNotifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/abyssNotifications.lua"))()
 
 lib.Notification.unnamedbuildinrobloxnotificationsbutmadeeasy = "This will be made lator yesyes"
 
 lib.UI = {}
 
-lib.UI.Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/Fluent.lua"))()
+lib.UI.Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/Fluent.lua"))()
 
-lib.UI["0x37"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/0x37.lua"))()
+lib.UI["0x37"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/0x37.lua"))()
 
-lib.LucideIcons = loadstring(game:HttpGet("https://raw.githubusercontent.com/R3P3x/FutureLib/refs/heads/main/libraries/LucideIcons.lua"))
+lib.LucideIcons = loadstring(game:HttpGet("https://raw.githubusercontent.com/SB-Script/FutureLib/refs/heads/main/libraries/LucideIcons.lua"))
 
 return lib
